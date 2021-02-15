11836d7c4e20d97a5a205ea9fdd19f682f829bfc
ghdtl123 <76501265+ghdtl123@users.noreply.github.com>
2021-02-15 14:17:32 +0900

    2102151417

---
+++ js/scroll.js
@@ -0,0 +1,29 @@
+(function($){
+    $.aniPage=function(e,type){
+        if(e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0){
+            $("body, html").animate({
+                scrollTop:$(window).scrollTop()-$(window).height()
+            },500,function(){
+                    $.aniOk = 0;
+            });
+        }else{
+            $("body, html").animate({
+                scrollTop:$(window).scrollTop()+$(window).height()
+            },500,function(){
+                    $.aniOk = 0;
+            });
+        }
+    };
+})(jQuery);
+$(function(){
+    $(window).height();
+    $.aniOk=0;
+    $(window).scrollTop();
+});
+$(document).on("mousewheel DOMMouseScroll",function(e){
+    e.preventDefault();
+    if($.aniOk == 0){
+        $.aniPage(e,e.type);
+        $.aniOk = 1;
+    }
+});
